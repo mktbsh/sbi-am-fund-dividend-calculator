9123bfedb7fcdb05224e836f8b6dfe434ba699c0
mktbsh <t93.hasebe@gmail.com>
2025-01-08 23:57:46 +0900

    eslint → biome

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,34 @@ import { TableRow } from "./components/table-row";
 import { data } from "./data";
 
 function App() {
-  return (
-    <div className="p-4 sm:px-6 lg:px-8">
-      <div className="sm:flex sm:items-center">
-        <div className="sm:flex-auto">
-          <h1 className="text-base font-semibold leading-6 text-gray-900">
-            SBI 年4回決算ファンド分配金計算
-          </h1>
-          <p className="mt-2 text-sm text-gray-700">
-            SBIアセットマネジメント株式会社が設定・運用する年4回決算ファンドの分配金計算用ツール
-          </p>
-        </div>
-      </div>
-      <div className="mt-8 flow-root">
-        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle">
-            <table className="min-w-full divide-y divide-gray-300">
-              <TableHead/>
-              <tbody className="divide-y divide-gray-200 bg-white">
-                {data.funds.map((fund) => (
-                  <TableRow key={fund.name} fund={fund} />
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="p-4 sm:px-6 lg:px-8">
+			<div className="sm:flex sm:items-center">
+				<div className="sm:flex-auto">
+					<h1 className="text-base font-semibold leading-6 text-gray-900">
+						SBI 年4回決算ファンド分配金計算
+					</h1>
+					<p className="mt-2 text-sm text-gray-700">
+						SBIアセットマネジメント株式会社が設定・運用する年4回決算ファンドの分配金計算用ツール
+					</p>
+				</div>
+			</div>
+			<div className="mt-8 flow-root">
+				<div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+					<div className="inline-block min-w-full py-2 align-middle">
+						<table className="min-w-full divide-y divide-gray-300">
+							<TableHead />
+							<tbody className="divide-y divide-gray-200 bg-white">
+								{data.funds.map((fund) => (
+									<TableRow key={fund.name} fund={fund} />
+								))}
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/lib.ts
+++ src/lib.ts
@@ -1,28 +1,21 @@
 import { NO_TAX, TAX } from "./data";
 
 export type CalcDividendParams = {
-  amount: number;
-  dividendYield: number;
-  isAfterTax: boolean;
-  type: "annual" | "quarter" | "monthly";
+	amount: number;
+	dividendYield: number;
+	isAfterTax: boolean;
+	type: "annual" | "quarter" | "monthly";
 };
 
 export function calcDividend({
-  amount,
-  dividendYield,
-  isAfterTax,
-  type,
-}: CalcDividendParams) {
-  const dividend = amount * dividendYield * (isAfterTax ? TAX : NO_TAX);
-  switch (type) {
-    case "quarter":
-      return floorValue(dividend / 4);
-    case "monthly":
-      return floorValue(dividend / 12);
-    case "annual":
-    default:
-      return floorValue(dividend);
-  }
+	amount,
+	dividendYield,
+	isAfterTax,
+	type,
+}: CalcDividendParams): number {
+	const dividend = amount * dividendYield * (isAfterTax ? TAX : NO_TAX);
+  const divider = type === "quarter" ? 4 : type === "monthly" ? 12 : 1;
+  return floorValue(dividend / divider);
 }
 
 const floorValue = (v: number) => Math.floor(v);
